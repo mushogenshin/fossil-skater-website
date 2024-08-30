57dde5560b5ae29d7bdd4d41112d75c05ad9c6ed
Mushogenshin <mushogenshin@gmail.com>
2024-08-30 17:12:16 +0700

    WIP: Nav bar needs Twitter and Facebook URLs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./index.css";
-import "./custom-forms.css";
+// import "./custom-forms.css";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <body className="leading-normal tracking-normal text-indigo-400 bg-cover bg-fixed bg-hero-pattern w-full h-screen">
       <div className="h-full">
-        <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
+        <Nav />
+        {/* <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
           Hello world!
-        </span>
+        </span> */}
       </div>
     </body>
   );
