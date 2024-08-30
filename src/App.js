cbd4f31ef608e34da3408f3fce54a78a1bc3a9ec
Mushogenshin <mushogenshin@gmail.com>
2024-08-30 16:10:30 +0700

    backgroundImage definition in theme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-// import logo from "./logo.svg";
-import "./App.css";
+import "./index.css";
 import "./custom-forms.css";
+import logo from "./logo.svg";
+// import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
-          Hello world!
-        </span>
-      </header>
+    <div className="leading-normal tracking-normal text-indigo-400 m-6 bg-cover bg-fixed bg-hero-pattern">
+      {/* <div className="App"> */}
+      <div className="h-full">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
+            Hello world!
+          </span>
+        </header>
+      </div>
     </div>
   );
 }
