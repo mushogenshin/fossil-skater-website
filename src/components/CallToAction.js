4d03551de9fbb5e1e14580937e3f944170d6af88
Mushogenshin <mushogenshin@gmail.com>
2024-08-30 19:10:31 +0700

    custom underlined "for free" text span

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,12 +1,14 @@
 import React from "react";
 import SteamLogo from "../images/logo_Steam.svg";
 import EpicStoreLogo from "../images/logo_EpicStore.svg";
+import "../styles/custom-styles.css";
 
 export default function CallToAction() {
   return (
     <div className="mx-auto md:pt-16">
       <p className="text-blue-400 font-bold pb-8 lg:pb-6 text-center">
-        Download our game:
+        Download our game&nbsp;
+        <span className="double-underline">for free:</span>
       </p>
       <div className="flex w-full justify-center md:justify-start pb-24 lg:pb-0 fade-in">
         {/* Steam */}
