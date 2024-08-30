c5d86660e8959474be1533772afda74416d26d7e
Mushogenshin <mushogenshin@gmail.com>
2024-08-30 16:58:46 +0700

    use print-color-adjust; delete App.css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import "./index.css";
 import "./custom-forms.css";
-import "./App.css";
-import logo from "./logo.svg";
 
 function App() {
   return (
-    <body className="leading-normal tracking-normal text-indigo-400 bg-cover bg-fixed bg-hero-pattern w-full h-screen m-0 p-0">
-      <div className="App">
-        <div className="h-full">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
-              Hello world!
-            </span>
-          </header>
-        </div>
+    <body className="leading-normal tracking-normal text-indigo-400 bg-cover bg-fixed bg-hero-pattern w-full h-screen">
+      <div className="h-full">
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
+          Hello world!
+        </span>
       </div>
     </body>
   );
