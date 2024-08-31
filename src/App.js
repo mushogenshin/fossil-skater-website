25195905f04e927b6cbf27bdb50234fbb75738ed
Mushogenshin <mushogenshin@gmail.com>
2024-08-31 13:05:44 +0700

    web title and preview content; background remove h-full

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import GameWindow from "./components/GameWindow";
 
 function App() {
   return (
-    <div className="leading-normal tracking-normal text-indigo-400 bg-cover bg-fixed bg-hero-pattern w-full h-screen">
-      <div className="h-full">
+    <div className="leading-normal tracking-normal text-indigo-400 bg-cover bg-fixed w-full h-screen">
+      <div className="bg-hero-pattern">
         <Nav />
         {/* Main */}
         <div className="container pt-24 md:pt-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
